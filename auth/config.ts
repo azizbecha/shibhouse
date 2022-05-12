0d87770d8dcd7849d575e28bf3da0f080ba2e1fb
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-05-12 13:07:41 +0200

    Delete config.ts

--- auth/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCDWEjD_clDNtM6jJCywW8SMFdK-tQ8zZs",
-    authDomain: "shibhouse.firebaseapp.com",
-    projectId: "shibhouse",
-    storageBucket: "shibhouse.appspot.com",
-    messagingSenderId: "154636035994",
-    appId: "154636035994:web:a46547d03c270584047c23",
-    measurementId: "G-X724MQ6G20"
-};
