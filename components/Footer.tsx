bcdf43732949fdd9c5ae3fcb0204472a444d870d
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-05-21 01:21:55 +0200

    fix footer text color in homepage

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,15 @@ const Footer = () => {
         <div>
             <footer className="text-center lg:text-left bg-darker text-gray-600">
                 <div className="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300">
-                    <div className="mr-12 lg:block font-inter font-bold text-lg">
+                    <div className="mr-12 lg:block font-inter font-bold text-lg text-white">
                         <span>Shibhouse</span>
                     </div>
                     <div className="flex justify-center space-x-4">
                         <Link href="report">
-                            <span className="mx-6 text-gray-600 font-semibold">Report a bug</span>
+                            <span className="mx-6 text-gray-600 font-semibold text-white">Report a bug</span>
                         </Link>
                         <Link href="privacy">
-                            <span className="mx-6 text-gray-600 font-semibold">Privacy Policy</span>
+                            <span className="mx-6 text-gray-600 font-semibold text-white">Privacy Policy</span>
                         </Link>
                     </div>
                 </div>
