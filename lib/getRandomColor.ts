f744ab9d63715f9559cadf257ad8352dc36aed6e
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-06-03 20:36:54 +0200

    fix props type

--- lib/getRandomColor.ts
+++ lib/getRandomColor.ts
@@ -1,4 +1,4 @@
-const getRandomColor = () => {
+const getRandomColor: () => string = () => {
     var color: string = Math.floor(Math.random() * 16777216).toString(16);
     return '#000000'.slice(0, -color.length) + color;
 }
