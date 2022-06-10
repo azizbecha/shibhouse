a400cd7adbb696ff5c92f79d9088ac08815eadd3
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-06-10 23:44:10 +0200

    added more icons in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
+import { FaBug, FaLock } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div>
             <footer className="text-center lg:text-left bg-darker text-gray-600">
                 <div className="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300">
-                    <div className="mr-12 lg:block font-inter font-bold text-lg text-white">
+                    <div className="mr-10 lg:block font-inter font-bold text-lg text-white">
                         <span>Shibhouse</span>
                     </div>
-                    <div className="flex justify-center space-x-4">
-                        <a href="https://github.com/azizbecha/shibhouse/issues" target={"_blank"} rel="noreferrer">
-                            <span className="mx-6 text-gray-600 font-semibold text-white">Report a bug</span>
+                    <div className="flex justify-center space-x-3">
+                        <a href="https://github.com/azizbecha/shibhouse/issues" className="mx-6 text-gray-600 font-semibold text-white flex space-x-1" target={"_blank"} rel="noreferrer">
+                            <FaBug className="my-auto" /> <span>Report a bug</span>
                         </a>
                         <Link href="privacy">
-                            <span className="mx-6 text-gray-600 font-semibold text-white">Privacy Policy</span>
+                            <span className="mx-6 text-gray-600 font-semibold text-white flex space-x-1"><FaLock className="my-auto" /> <span>Privacy Policy</span></span>
                         </Link>
                     </div>
                 </div>
