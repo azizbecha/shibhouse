97e400ef483ab2f4819bac1aba3eab3f5601e315
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-06-21 01:24:14 +0200

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+import { FaArrowLeft } from "react-icons/fa"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+const NotFound: React.FC = () => {
+    return (
+        <>
+            <Navbar />
+            <div
+                className="
+                    flex
+                    items-center
+                    justify-center
+                    w-screen
+                    h-screen
+                    bg-dark
+                "
+                >
+                <div className="px-28 py-14 bg-darker text-white rounded-md shadow-xl">
+                    <div className="flex flex-col items-center">
+                    <h1 className="font-bold text-white text-8xl">404</h1>
+
+                    <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+                        Oops ! Page not found
+                    </h6>
+
+                    <p className="mb-6 text-center text-gray-500 md:text-lg">
+                        The page you’re looking for doesn’t exist.
+                    </p>
+
+                    <Link href="/">
+                        <button className="bg-primary px-6 py-2 text-white font-semibold rounded-lg flex">
+                            <FaArrowLeft className="my-auto mr-1" /> Go back
+                        </button>
+                    </Link>
+                    
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default NotFound
