5a2226a98fc198d212cc784b7557060af6de75a3
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-06-22 20:17:57 +0200

    renamed the mobile app

--- poppy/ios/ShibhouseTests/ShibhouseTests.m
+++ poppy/ios/ShibhouseTests/ShibhouseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface poppyTests : XCTestCase
+@interface ShibhouseTests : XCTestCase
 
 @end
 
-@implementation poppyTests
+@implementation ShibhouseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
