3966216909439c2eddf367f7321aeb93e1f31d06
Aziz Becha <63454940+azizbecha@users.noreply.github.com>
2022-06-25 01:22:27 +0200

    poppy: added PrivateRoute component

--- poppy/pages/Home/index.tsx
+++ poppy/pages/Home/index.tsx
@@ -1,51 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { SafeAreaView } from "react-native";
 
-import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
-
+import auth from "@react-native-firebase/auth";
 import { Button, Text } from "react-native-paper";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
+import Toast from "react-native-toast-message";
+
+import PrivateRoute from "../../routes/PrivateRoute";
 import { Colors } from "../../config";
 
 const Home: React.FC = () => {
-
-    const navigation = useNavigation<StackNavigationProp<any>>();
-
-    const [initializing, setInitializing] = useState(true);
-    const [userData, setUserData] = useState<FirebaseAuthTypes.User | null>(null);
-
-    // Handle user state changes
-    function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
-        setUserData(user);
-        if (initializing) setInitializing(false);
-    }
-
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber; // unsubscribe on unmount
-    }, []);
-
-    if (initializing) return null;
-
-    if (!userData) {
-        navigation.navigate("Login")
-    }
-
     const logOut = () => {
         auth()
         .signOut()
         .then(() =>{
-            navigation.navigate("Login")
+            Toast.show({
+                type: 'success',
+                text1: 'Success',
+                text2: 'Logged out successfully',
+                position: 'bottom',
+            });
         });
     }
     
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
-            <Text>Logged in as: {userData?.email}</Text>
-            <Button color={Colors.primary} mode='contained' onPress={() => logOut()}>Log out</Button>
-        </SafeAreaView>    
+        <PrivateRoute>
+            <SafeAreaView>
+                <Text>Home</Text>
+                <Button color={Colors.primary} mode='contained' onPress={() => logOut()}>Log out</Button>
+            </SafeAreaView>
+        </PrivateRoute>
     )
 }
 
---
+++ poppy/routes/PrivateRoute.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<Props> = ({children}) => {
+
+    const navigation = useNavigation<StackNavigationProp<any>>();
+
+    const [initializing, setInitializing] = useState(true);
+    const [userData, setUserData] = useState<FirebaseAuthTypes.User | null>(null);
+
+    // Handle user state changes
+    function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+        setUserData(user);
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    if (initializing) return null;
+
+    if (!userData) {
+        navigation.navigate("Login")
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default PrivateRoute
