9a5302b62d3d237502169f9b87c5ed7b9cf67a9b
Aziz Becha <aziz07becha@gmail.com>
2022-10-11 22:53:50 +0100

    fixed date in notification

--- pixie/lib/createNotification.ts
+++ pixie/lib/createNotification.ts
@@ -4,13 +4,10 @@ import generateId from "./generateId";
 
 export const createNotification = async (text: string) => {
     const id: string = generateId(8);
-    var today: Date = new Date();
-    var date = today.toJSON().slice(0, 10);
-    var finalDate = date.slice(8, 10) + '/' + date.slice(5, 7) + '/' + date.slice(0, 4);
 
     const docRef = await setDoc(doc(fireStore, "notifications", id), {
         id: id,
         text: text,
-        date: finalDate
+        date: Date.now().toString()
     });
 }
