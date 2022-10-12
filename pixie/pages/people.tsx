268ca6aa87d5fc268db7f90df3c9cdd0cffa2c24
Aziz Becha <aziz07becha@gmail.com>
2022-10-12 18:16:18 +0100

    improved users cards in people page

--- pixie/pages/people.tsx
+++ pixie/pages/people.tsx
@@ -150,7 +150,7 @@ const People: React.FC = () => {
                                             return (
                                                 <Col xs={12} sm={6} md={4} lg={4} key={key} className="userElement">
                                                     <div className="bg-darker px-4 py-6 rounded-md shadow-lg mb-3 justify-start items-center flex">
-                                                        <div className="flex items-center space-x-4">
+                                                        <div className="flex items-center space-x-4 py-2">
                                                             <Link href={`user/${user.username}`}>
                                                                 <div className="flex-shrink-0 cursor-pointer">
                                                                     <div className="p-4 text-white rounded-full font-bold" style={{backgroundColor: user.avatarColor}}>
@@ -159,14 +159,14 @@ const People: React.FC = () => {
                                                                 </div>
                                                             </Link>
                                                             
-                                                            <div className="cursor-pointer">
+                                                            <div className="cursor-pointer -mt-3">
                                                                 <Link href={`user/${user.username}`}>
                                                                     <div>
                                                                         <h2 className="text-white font-semibold text-xl">{capitalizeWord(user.firstname)} {capitalizeWord(user.lastname)}</h2>
                                                                         <h3 className="text-white font-normald text-md">@{user.username}</h3>
                                                                     </div>
                                                                 </Link>
-                                                                <div className="text-sm mt-2 text-gray-200">
+                                                                <div className="text-sm mt-3.5 text-gray-200">
                                                                     <div className="flex flex-row ml-auto space-x-6 items-center">
                                                                         <div className="mb-1 h-5 w-20">{user.followers.includes(currentUserData.username) ? <button onClick={() => Unfollow(user)} className="bg-primary text-white px-4 py-2 rounded-md font-semibold flex"><FaTimes className="my-auto mr-1" /> Unfollow</button> : <button onClick={() => Follow(user)} className="bg-primary text-white px-4 py-2 rounded-md font-semibold flex"><FaPlus className="my-auto mr-1" /> Follow</button>}</div>
                                                                     </div>
