9e1c80302a1440ca7e81fd9c9a12e32b8ce265cd
Aziz Becha <aziz07becha@gmail.com>
2022-10-19 13:09:46 +0100

    fixed middle column height in dashboard

--- pixie/pages/dashboard.tsx
+++ pixie/pages/dashboard.tsx
@@ -178,7 +178,7 @@ const Dashboard: React.FC = () => {
                                 <PeopleSidebar />
                             </Col>
                             <Col xs={12} sm={3} md={10} lg={6}>
-                                <main className="flex flex-col w-full bg-darker overflow-x-hidden overflow-y-auto rounded-lg mb-14">
+                                <main className="flex h-full flex-col w-full bg-darker overflow-x-hidden overflow-y-auto rounded-lg mb-14">
                                     <Transition.Root show={showModal} as={Fragment}>
                                         <Dialog as="div" className="relative z-50" initialFocus={cancelButtonRef} onClose={setShowModal}>
                                             <Transition.Child
