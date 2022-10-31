2e5b27f1b35a4376cf38d5ceec9ff98006eab974
Aziz Becha <aziz07becha@gmail.com>
2022-10-31 22:22:49 +0100

    initialized storage object

--- pixie/auth/Firebase.ts
+++ pixie/auth/Firebase.ts
@@ -1,6 +1,7 @@
 import { FirebaseApp, initializeApp } from "firebase/app"
 import { Auth, getAuth } from "firebase/auth"
 import { Firestore, getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import { firebaseConfig } from "./config";
 
 // Initialize Firebase
@@ -8,3 +9,4 @@ const app: FirebaseApp = initializeApp(firebaseConfig);
 
 export const auth: Auth = getAuth(app);
 export const fireStore: Firestore = getFirestore(app);
+export const storage = getStorage(app);
